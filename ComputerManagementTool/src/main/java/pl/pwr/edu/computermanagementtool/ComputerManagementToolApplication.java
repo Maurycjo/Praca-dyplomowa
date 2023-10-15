9b3a2675133677e5860cc7da4e68909b1185a200
Maurycy Niewczas <maurycy.niewczas@gmail.com>
2023-10-15 11:13:26 +0200

    utworzenie projektu

---
+++ ComputerManagementTool/src/main/java/pl/pwr/edu/computermanagementtool/ComputerManagementToolApplication.java
@@ -0,0 +1,13 @@
+package pl.pwr.edu.computermanagementtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerManagementToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComputerManagementToolApplication.class, args);
+    }
+
+}
