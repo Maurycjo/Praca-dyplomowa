2bb1096d2ba904ca70cf3a86497fa655a1cfbd70
Maurycy Niewczas <maurycy.niewczas@gmail.com>
2023-11-21 13:09:52 +0100

    dodanie adnotacji reposotory do repozytorium

--- ComputerManagementTool/src/main/java/pl/pwr/edu/computermanagementtool/repository/ParticipationRepository.java
+++ ComputerManagementTool/src/main/java/pl/pwr/edu/computermanagementtool/repository/ParticipationRepository.java
@@ -1,7 +1,9 @@
 package pl.pwr.edu.computermanagementtool.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.pwr.edu.computermanagementtool.entity.Participation;
 
+@Repository
 public interface ParticipationRepository extends JpaRepository<Participation, Integer> {
 }
